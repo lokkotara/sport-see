0967c953c7d07cf794492bd7ddf0cce53534ce3a
greg <lokkotarakun@gmail.com>
2022-07-08 08:45:32 +0200

    feat: amélioration du rendu de la page d'erreur

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Error.scss";
 
 export default function Error() {
   return (
     <main className="errorContainer">
-      <div className="errorUpperPart">
+      <div className="errorText">
         <h1>404</h1>
         <p>Oups ! La page que vous demandez n'existe pas.</p>
+        <Link to="/">Retour à l'accueil</Link>
       </div>
     </main>
   );
